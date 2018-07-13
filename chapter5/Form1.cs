9790e73dd0bec7f2d429ab30c54cd76ba0f57143
Jack Wilson <battlejackmaster@gmail.com>
2018-07-13 14:47:37 -0700

    Declared dinner party object in form

--- chapter5/Form1.cs
+++ chapter5/Form1.cs
@@ -12,9 +12,15 @@ namespace chapter5
 {
     public partial class Form1 : Form
     {
+        DinnerParty dinnerParty;
+
         public Form1()
         {
             InitializeComponent();
+            dinnerParty = new DinnerParty() { NumberOfPeople = 5 };
+            dinnerParty.SetHealthyOption(false);
+            dinnerParty.CalculateCostOfDecorations(true);
+            DisplayDinnerPartyCost();
         }
     }
 }
