a189ed4bd2e30ba9cfa2693d4a2d78e79d5eb956
Jack Wilson <battlejackmaster@gmail.com>
2018-07-13 14:44:57 -0700

    Added dinner party classes and filled in varialbes + methods

---
+++ chapter5/DinnerParty.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter5
+{
+    class DinnerParty
+    {
+        int NumberOfPeople;
+        decimal CostOfBeveragesPerPerson;
+        decimal CostOfDecorations;
+        public const int CostOfFoodPerPerson = 25;
+
+        public void SetHealthyOption(bool healthyOption)
+        {
+            if (healthyOption)
+            {
+                CostOfBeveragesPerPerson = 5.00M;
+            }
+            else
+            {
+                CostOfBeveragesPerPerson = 20.00M;
+            }
+        }
+
+        public void CalculateCostOfDecorations(bool fancy)
+        {
+            if (fancy)
+            {
+                CostOfDecorations = (NumberOfPeople * 15.00M) + 50M;
+            }
+            else
+            {
+                CostOfDecorations = (NumberOfPeople * 7.50M) + 30M;
+            }
+        }
+
+        public decimal CalculateCost(bool healthyOption)
+        {
+            decimal totalCost = CostOfDecorations + ((CostOfBeveragesPerPerson + CostOfFoodPerPerson) * NumberOfPeople);
+
+            if (healthyOption)
+            {
+                return totalCost * .95M;
+            }
+            else
+            {
+                return totalCost;
+            }
+        }
+    }
+}
