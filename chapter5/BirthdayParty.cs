e780be6a4950ca5494c92a502aa14348f96cc2b1
Jack Wilson <battlejackmaster@gmail.com>
2018-07-13 17:33:49 -0700

    Added event handlers. Also added more methods to birthday party class.

--- chapter5/BirthdayParty.cs
+++ chapter5/BirthdayParty.cs
@@ -50,5 +50,41 @@ namespace chapter5
             else
                 return 40;
         }
+
+        public bool CakeWritingTooLong
+        {
+            get
+            {
+                if (CakeWriting.Length > MaxWritingLength())
+                    return true;
+                else
+                    return false;
+            }
+        }
+
+        private decimal CalculateCostOfDecorations()
+        {
+            decimal costOfDecorations;
+            if (FancyDecorations)
+                costOfDecorations = (NumberOfPeople * 15.00M) + 50M;
+            else
+                costOfDecorations = (NumberOfPeople * 7.50M) + 30M;
+            return costOfDecorations;
+        }
+
+        public decimal Cost
+        {
+            get
+            {
+                decimal totalCost = CalculateCostOfDecorations();
+                totalCost += CostOfFoodPerPerson * NumberOfPeople;
+                decimal cakeCost;
+                if (CakeSize() == 8)
+                    cakeCost = 40M + ActualLength * .25M;
+                else
+                    cakeCost = 75M + ActualLength * .25M;
+                return totalCost + cakeCost;
+            }
+        }
     }
 }
